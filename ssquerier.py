272de84a6b93e8a2871daced659e972842699d3b
Aaron Swoiskin <aswoiskin@gmail.com>
2019-02-17 19:59:17 -0800

    darn

--- ssquerier.py
+++ ssquerier.py
@@ -24,22 +24,27 @@ class Participant:
 	def partial_interpolate(self, all_participants):
 		points = [p.share for p in all_participants]
 		v = 0
+		print "keys2: ", points
 		for j in range(len(points)):
 			y = points[j][1]
-			l = self.basis(points, 0, j)
+			l = self.basis(points, self.share[0], j)
 			v += y*l
 		v = v % self.q
-		return self.mod_pow(self.g, v, self.p)
+		return mod_pow(self.g, v, self.p)
 
 	# jth basis at x 
 	def basis(self, points, x, j):
 		val = 1
 		for m in range(len(points)):
 			if not m == j:
-				a = (x - points[m][0]) 
-				b = (points[j][0] - points[m][0])
-				v=Fraction(a)/Fraction(b)
-				val *= v
+				a = x - points[m][0]
+				b = points[j][0] - points[m][0]
+				
+				if b == 0:
+					print "\nFUCK: ", points[j][0], points[m][0], " || ", j, m
+				else: 
+					v = Fraction(a)/Fraction(b)
+					val *= v
 		return val
 
 
@@ -59,67 +64,71 @@ class Participant:
 		return actual == guess
 
 
-	# val: the secret
-	# k: the number of valid participant shares needed to recover the secret
-	# q: the prime you mod by as you'll be working in mod q
-	def distribute_shares(self, all_participants, val, k, q):
-		r = 1000
-		neg = [1, -1]
-		coefs = [choice(neg) * Fraction(randint(1, r), randint(1, r)) for i in range(0, k-1)]
-		coefs.insert(0, val)
+# val: the secret
+# k: the number of valid participant shares needed to recover the secret
+# q: the prime you mod by as you'll be working in mod q
+def distribute_shares(all_participants, val, k, q):
+	print "sec: ", val
+	r = 1000
+	neg = [1, 1] #################
+	coefs = [choice(neg) * Fraction(randint(1, r), randint(1, r)) for i in range(0, k-1)]
+	coefs.insert(0, val)
 
-		xs = []
-		ys = []
-		for i in range(k):
-			p = 0
-			fp = 0
-			while p == 0 or p in xs or fp == 0:
-				p = randint(-r, r)
-				fp = polyVal(coefs, p) % q
+	keys = set([])
+	
+	while len(keys) < len(all_participants):
+		p = randint(1, r)
+		fp = polyVal(coefs, p)
+		keys.add((p % q, fp % q))
 
-			xs.append(p)
-			ys.append(fp) #?
-		
-		keys = [(xs[i], ys[i]) for i in range(len(xs))]
+	print "keys1: ", keys
 
-		for i in range(len(all_participants)):
-			all_participants[i].share = keys[i]
+	for i in range(len(all_participants)):
+		all_participants[i].share = keys.pop()
 
-	# given list of coefficents for p, find p(x)
-	def polyVal(self, coefs, x):
-		val = 0
-		i = 0
-		for c in coefs:
-			val += c * (x**i)
-			i += 1
-		return val
+# given list of coefficents for p, find p(x)
+def polyVal(coefs, x):
+	val = 0
+	i = 0
+	for c in coefs:
+		val += c * (x**i)
+		i += 1
+	return val
 
-	# computes the greatest common divisor of a and b
-	# Euclidian algorithm
-	def gcd(self, a, b):
-		r = a % b
-		if r == 0:
-			return b
-		else:
-			return self.gcd(b, r)
+# computes the greatest common divisor of a and b
+# Euclidian algorithm
+def gcd(a, b):
+	r = a % b
+	if r == 0:
+		return b
+	else:
+		return gcd(b, r)
 
-	# computes b to the p-th power mod q
-	# must be done in logarithmic time complexity
-	# note: don't do b ** p % q; this could overflow with very large numbers
-	def mod_pow(self, b, p, q):
-		total = 1
-		current = b
+# computes b to the p-th power mod q
+# must be done in logarithmic time complexity
+# note: don't do b ** p % q; this could overflow with very large numbers
+def mod_pow(b, p, q):
+	total = 1
+	current = b
 
-		binary = bin(int(p))
-		binary = binary[2:]
-		binary = binary[::-1]
+	binary = bin(int(p))
+	binary = binary[2:]
+	binary = binary[::-1]
 
-		for bit in binary:
-			if bit == '1':
-				total = (total * current) % q
-			current = (current ** 2) % q
-		return total
+	for bit in binary:
+		if bit == '1':
+			total = (total * current) % q
+		current = (current ** 2) % q
+	return total
 
-	# computes the inverse of x mod q
-	def inverse(self, x, q):
-		return self.mod_pow(x, q-2, q)
+# computes the inverse of x mod q
+def inverse(x, q):
+	if q == 1:
+		return 0
+
+	for i in range(1, q):
+		if (i * x) % q == 1:
+			print i
+			return i
+
+	return None
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from ssquerier import Participant
+from ssquerier import inverse
 from random import randint, choice
 
 if __name__ == "__main__":
 	temp = Participant(1,2,3,4,5)
-	val = temp.mod_pow(100,20, 175)
+	val = inverse(43287, 3617)
 	print val
